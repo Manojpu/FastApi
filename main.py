82712650b7cb1ad532d2fe7e23c46ac6561f9d12
Prashant-Pubudu <100054944+Manojpu@users.noreply.github.com>
2025-04-22 20:02:17 +0530

    Enhance about route to include published parameter for conditional responses

--- main.py
+++ main.py
@@ -11,11 +11,17 @@ def index():
     }}
 
 @app.get("/about")
-def about(limit,ex):
-    return {"data":{
+def about(limit,ex,published):
+    if published == "true":
+       return {"data":{
         "about": f"{limit} {ex} I am a software engineer with a passion for learning new technologies and solving complex problems. I have experience in web development, data analysis, and machine learning. I enjoy working on projects that challenge me and allow me to grow as a developer."
     }}
 
+    else:
+        return{
+            "data": f"{limit} unpublished blogs"
+        }
+    
 @app.get("/blog/{id}")
 def show(id: int):
     return {"data":id}
