cc5edf52e0b46cf5be8ab9b1b5ac77243d29ff61
Prashant-Pubudu <100054944+Manojpu@users.noreply.github.com>
2025-04-22 11:49:09 +0530

    Add FastAPI application with basic routes and .gitignore file

---
+++ main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def index():
+    return {"data":{
+        "name": "Pubudu",
+        "age": 22,
+        "country": "Sri Lanka"
+    }}
+
+@app.get("/about")
+def about():
+    return {"data":{
+        "about": "I am a software engineer with a passion for learning new technologies and solving complex problems. I have experience in web development, data analysis, and machine learning. I enjoy working on projects that challenge me and allow me to grow as a developer."
+    }}
