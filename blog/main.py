2c41ab6de3435642a607aae9d86ba30b913f9a66
Prashant-Pubudu <100054944+Manojpu@users.noreply.github.com>
2025-04-23 00:04:09 +0530

    Implement blog model, database setup, and create blog schema

--- blog/main.py
+++ blog/main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
+from .import schemas,model
+from database import engine
 
+model.Base.metadata.create_all(engine)
 app = FastAPI()
 
 @app.post("/blog")
-def create_blog():
-    return "creating"
+def create_blog(request: schemas.Blog):
+    return schemas.Blog
---
+++ blog/model.py
@@ -0,0 +1,10 @@
+from database import Base
+from sqlalchemy import Column, Integer, String, Boolean
+
+class Blog(Base):
+    __tablename__ = "blogs"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True)
+    body = Column(String, index=True)
+    published = Column(Boolean, default=True)
