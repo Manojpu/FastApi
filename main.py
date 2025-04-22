906e18d4aa32d8cc9e12ec9a0c87b2aca458b1f7
Prashant-Pubudu <100054944+Manojpu@users.noreply.github.com>
2025-04-22 18:58:12 +0530

    Add blog route to FastAPI application

--- main.py
+++ main.py
@@ -15,3 +15,7 @@ def about():
     return {"data":{
         "about": "I am a software engineer with a passion for learning new technologies and solving complex problems. I have experience in web development, data analysis, and machine learning. I enjoy working on projects that challenge me and allow me to grow as a developer."
     }}
+
+@app.get("/blog/{id}")
+def show(id):
+    return {"data":id}
