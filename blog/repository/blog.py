488a318b838b8218885b322082eeeb63998903aa
Prashant-Pubudu <100054944+Manojpu@users.noreply.github.com>
2025-04-24 10:43:16 +0530

    Refactor blog functionality; move blog retrieval and deletion logic to repository module for improved organization and maintainability

--- blog/repository/blog.py
+++ blog/repository/blog.py
@@ -1,4 +1,5 @@
-from .. import model  
+from .. import model
+from fastapi import HTTPException, status 
 from sqlalchemy.orm import Session
 def get_all(db: Session):
     blogs = db.query(model.Blog).all()
@@ -10,3 +11,39 @@ def create(request: model.Blog, db: Session):
     db.commit()
     db.refresh(new_blog)
     return new_blog
+
+
+
+def get_blog_with_id(id: int, db: Session):
+    blog = db.query(model.Blog).filter(model.Blog.id == id).first()
+    if not blog:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Blog with id {id} not found"
+        )
+    return blog
+
+def delete_blog(id: int, db: Session):
+    blog = db.query(model.Blog).filter(model.Blog.id == id).first()
+    if not blog:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Blog with id {id} not found"
+        )
+    db.delete(blog)
+    db.commit()
+    return {"message": f"Blog with id {id} deleted successfully."}
+
+def update_blog(id: int, request: model.Blog, db: Session):
+    blog_query = db.query(model.Blog).filter(model.Blog.id == id)
+    blog = blog_query.first()
+    
+    if not blog:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Blog with id {id} not found"
+        )
+    
+    blog_query.update(request.dict(), synchronize_session=False)
+    db.commit()
+    return {"message": f"Blog with id {id} updated successfully."}
--- blog/routers/blog.py
+++ blog/routers/blog.py
@@ -19,44 +19,16 @@ def get_blogs_all(db: Session = Depends(get_db)):
 def create_blog(request: schemas.Blog, db: Session = Depends(get_db)):
     return blog.create(request, db)
 
-@router.get("/", response_model=List[schemas.ShowBlog])
-def get_blogs(db: Session = Depends(get_db)):
-    blogs = db.query(model.Blog).all()
-    return blogs
-
 @router.get("/{id}", response_model=schemas.ShowBlog)
-def get_blog(id: int, response: Response, db: Session = Depends(get_db)):
-    blog = db.query(model.Blog).filter(model.Blog.id == id).first()
-    if not blog:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail=f"Blog with id {id} not found"
-        )
-    return blog
+def get_blog_with_id(id: int, response: Response, db: Session = Depends(get_db)):
+
+    return blog.get_blog_with_id(id, db)
 
 @router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_blog(id: int, db: Session = Depends(get_db)):
-    blog = db.query(model.Blog).filter(model.Blog.id == id).first()
-    if not blog:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail=f"Blog with id {id} not found"
-        )
-    db.delete(blog)
-    db.commit()
-    return Response(status_code=status.HTTP_204_NO_CONTENT)
+  
+    return blog.delete_blog(id, db)
 
 @router.put("/{id}", status_code=status.HTTP_202_ACCEPTED)
 def update_blog(id: int, request: schemas.Blog, db: Session = Depends(get_db)):
-    blog_query = db.query(model.Blog).filter(model.Blog.id == id)
-    blog = blog_query.first()
-    
-    if not blog:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail=f"Blog with id {id} not found"
-        )
-    
-    blog_query.update(request.dict(), synchronize_session=False)
-    db.commit()
-    return {"message": f"Blog with id {id} updated successfully."}
+    return blog.update_blog(id, request, db)
